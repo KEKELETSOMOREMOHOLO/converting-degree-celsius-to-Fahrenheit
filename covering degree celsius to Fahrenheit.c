e2f139f026d68262310946743d5e8f3f43b0a7dc
KEKELETSOMOREMOHOLO <118248938+KEKELETSOMOREMOHOLO@users.noreply.github.com>
2023-02-18 12:29:54 +0530

    Create covering degree celsius to Fahrenheit.c

---
+++ covering degree celsius to Fahrenheit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  float fahrenheit, celsius;
+  printf("Please enter the temperature in degree celsius: \n");
+  scanf("5f",&celsius);
+  printf(\n\nTemperature in fahrenheit is: %f",fahrenheit);
+  return 0;
+}
